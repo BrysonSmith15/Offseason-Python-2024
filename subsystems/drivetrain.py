5f72c80e4ead477c0101594e85cdc9e9b72014f7
Bryson Smith <15smith.bryson@gmail.com>
2024-03-21 21:49:47 -0500

    Worked on drivetrain

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -1,12 +1,18 @@
 from commands2 import SubsystemBase
+from hal import SerialPort
 from ntcore import NetworkTableInstance
 from wpilib import RobotBase, RobotState, SmartDashboard
-from wpimath.kinematics import SwerveModulePosition, SwerveModuleState
+from wpimath.kinematics import (
+    SwerveModulePosition,
+    SwerveModuleState,
+    SwerveDrive4Kinematics,
+)
 from wpimath.controller import HolonomicDriveController
 from wpimath.geometry import Translation2d, Rotation2d
 from wpiutil import *
 from navx import AHRS
 import math
+import swerve_module
 
 # MPS
 maxVelocity = 3.7
@@ -36,3 +42,25 @@ class Drivetrain(SubsystemBase):
     __ntTbl__ = NetworkTableInstance.getDefault().getTable("Drivetrain")
     fieldRelative: BooleanProperty = BooleanProperty("fieldRelative", True)
     holonomic_PID: HolonomicDriveController = None
+
+    def __init__(self):
+        super().__init__()
+        self.subsystem("Drivetrain")
+        self.setName("Drivetrain")
+
+        self.robotName = "Robot"
+        self.gyro = AHRS(serial_port_id=SerialPort.USB1)
+
+        self.fl = swerve_module.SwerveModule("fl", 14, 12, 13, True, True)
+        self.fr = swerve_module.SwerveModule("fr", 11, 9, 10, False, True)
+        self.bl = swerve_module.SwerveModule("bl", 17, 15, 16, True, True)
+        self.br = swerve_module.SwerveModule("br", 8, 6, 7, False, True)
+
+        self.addChild("Gryo", self.gyro)
+
+        self.kinematics = SwerveDrive4Kinematics(
+            self.fl.getModulePosition(),
+            self.fr.getModulePosition(),
+            self.bl.getModulePosition(),
+            self.br.getModulePosition(),
+        )
