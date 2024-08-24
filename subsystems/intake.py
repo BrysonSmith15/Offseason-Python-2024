f92256859dfc7457894d7b3b141adcea97d1a618
Bryson Smith <15smith.bryson@gmail.com>
2024-08-24 12:24:54 -0500

    Created intake functional commands

--- subsystems/intake.py
+++ subsystems/intake.py
@@ -1,6 +1,8 @@
-from commands2 import Subsystem
+from commands2 import Subsystem, FunctionalCommand, InterruptionBehavior
 from rev import CANSparkLowLevel, CANSparkMax
 from wpimath.filter import SlewRateLimiter
+from wpilib import RobotBase
+import typing
 
 
 class Intake(Subsystem):
@@ -11,15 +13,47 @@ class Intake(Subsystem):
 
         self.setName("Intake")
 
-        self.motor.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus2, 500)
-        self.motor.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus3, 500)
-        self.motor.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus4, 500)
-        self.motor.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus5, 500)
-        self.motor.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus6, 500)
+        self.curr_speed: float = 0
+
+        self.motor.setPeriodicFramePeriod(
+            CANSparkLowLevel.PeriodicFrame.kStatus2, 500)
+        self.motor.setPeriodicFramePeriod(
+            CANSparkLowLevel.PeriodicFrame.kStatus3, 500)
+        self.motor.setPeriodicFramePeriod(
+            CANSparkLowLevel.PeriodicFrame.kStatus4, 500)
+        self.motor.setPeriodicFramePeriod(
+            CANSparkLowLevel.PeriodicFrame.kStatus5, 500)
+        self.motor.setPeriodicFramePeriod(
+            CANSparkLowLevel.PeriodicFrame.kStatus6, 500)
 
     def set_motor(self, power: float) -> None:
         power = 1 if power > 1 else -1 if power < -1 else power
+        if RobotBase.isReal():
+            power = self.limiter.calculate(power)
         self.motor.set(self.limiter.calculate(power))
+        self.curr_speed = power
+
+    def run_motor(
+        self, * speed: typing.Callable[[], float],
+        can_run=lambda: True
+    ) -> FunctionalCommand:
+        out = FunctionalCommand(
+            onInit=lambda: self.set_motor(0),
+            onExecute=lambda: self.set_motor(speed() if can_run() else 0),
+            isFinished=lambda: False,
+            onEnd=lambda _interrupted: self.set_motor(0),
+        ).withInterruptBehavior(InterruptionBehavior.kCancelSelf)
+        out.addRequirements(self)
+        return out
+
+    def full_intake(self) -> FunctionalCommand:
+        return self.run_motor(speed=lambda: 1.0)
+
+    def slow_intake(self) -> FunctionalCommand:
+        return self.run_motor(speed=lambda: 0.1)
+
+    def reverse_intake(self) -> FunctionalCommand:
+        return self.run_motor(speed=lambda: -0.25)
 
     def stop(self) -> None:
         self.motor.set(0)
